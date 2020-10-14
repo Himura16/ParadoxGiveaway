fee3e326691a9a397bf60e4cdbc2627fbe1c6adf
Himura16 <70828450+Himura16@users.noreply.github.com>
2020-10-14 23:53:48 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = (client) => {
     `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
   );
 
-  const activities = [`Giveaways in ${client.guilds.cache.size} guilds`,"g!help",`over ${client.users.cache.size} users!`];
+  const activities = [`Live in ${client.guilds.cache.size} Servers`,"g!help",`Support Paradox`];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
     client.user.setActivity(activity, { type: "WATCHING" });
